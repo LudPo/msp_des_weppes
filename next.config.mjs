e28114a70368fd8edf9de378fc36012458bc1952
LudPo <lpotier@centralelille.lan>
2024-11-06 15:26:30 +0100

    image gh error fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
-    assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
+    // basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
+    // assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
+    basePath: '/msp_des_weppes',
+    assetPrefix: '/msp_des_weppes',
 };
 
 export default nextConfig;
